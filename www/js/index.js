130f6857469cceea1dc7ca9777d267333d674230
virat59 <virat59@gmail.com>
2015-05-15 11:35:23 +0530

    Commit 29
    Commit 29

--- www/js/index.js
+++ www/js/index.js
@@ -47,15 +47,15 @@ var app = {
         pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"74320630987","ecb":"app.onNotificationGCM"});
 		
 		//Database Changes
-		/* if (!window.openDatabase) {
+		if (!window.openDatabase) {
 				// not all mobile devices support databases  if it does not, the following alert will display
 				// indicating the device will not be albe to run this application
 			alert('Databases are not supported in this browser.');
 			return;
-		} */
+		}
 			// this line tries to open the database base locally on the device
 			// if it does not exist, it will create it and return a databasev object stored in variable db
-		/*db = openDatabase(shortName, version, displayName,maxSize);
+		db = openDatabase(shortName, version, displayName,maxSize);
 			// this line will try to create the table User in the database just created/openned
 		db.transaction(function(tx){
 				// you can uncomment this next line if you want the User table to be empty each time the application runs
@@ -64,7 +64,7 @@ var app = {
 				// note the UserId column is an auto incrementing column which is useful if you want to pull back distinct rows
 				// easily from the table.
 			tx.executeSql( 'CREATE TABLE IF NOT EXISTS tnet_login_details(Id INTEGER NOT NULL PRIMARY KEY, key TEXT NOT NULL, value TEXT NOT NULL)',[],nullHandler,errorHandler); 
-		},errorHandler,successCallBack);*/
+		},errorHandler,successCallBack);
     },
 	
     // Update DOM on a Received Event
