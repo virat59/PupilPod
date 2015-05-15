578ffff822f68a15f40d56ac82a9478262878157
virat59 <virat59@gmail.com>
2015-05-15 10:52:13 +0530

    Commit 20
    Commit 20

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,11 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var db;
+var shortName = 'tnet_pupilpod';
+var version = '1.0';
+var displayName = 'Tnet_Pupilpod';
+var maxSize = 65535;
 var app = {
     // Application Constructor
     initialize: function() {
@@ -30,7 +35,6 @@ var app = {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
-    //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
@@ -38,7 +42,26 @@ var app = {
         app.receivedEvent('deviceready');
         var pushNotification = window.plugins.pushNotification;
         pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"74320630987","ecb":"app.onNotificationGCM"});
-
+		
+		//Database Changes
+		if (!window.openDatabase) {
+				// not all mobile devices support databases  if it does not, the following alert will display
+				// indicating the device will not be albe to run this application
+			alert('Databases are not supported in this browser.');
+			return;
+		}
+			// this line tries to open the database base locally on the device
+			// if it does not exist, it will create it and return a databasev object stored in variable db
+		db = openDatabase(shortName, version, displayName,maxSize);
+			// this line will try to create the table User in the database just created/openned
+		db.transaction(function(tx){
+				// you can uncomment this next line if you want the User table to be empty each time the application runs
+				// tx.executeSql( 'DROP TABLE User',nullHandler,nullHandler);
+				// this line actually creates the table User if it does not exist and sets up the three columns and their types
+				// note the UserId column is an auto incrementing column which is useful if you want to pull back distinct rows
+				// easily from the table.
+			tx.executeSql( 'CREATE TABLE IF NOT EXISTS tnet_login_details(Id INTEGER NOT NULL PRIMARY KEY, key TEXT NOT NULL, value TEXT NOT NULL)',[],nullHandler,errorHandler); 
+		},errorHandler,successCallBack);
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -53,7 +76,18 @@ var app = {
     },
     // result contains any message sent from the plugin call
     successHandler: function(result) {
-        alert('Callback Success! Result = '+result)
+        //alert('Callback Success! Result = '+result)
+		if (!window.openDatabase) {
+			alert('Databases are not supported in this browser.');
+			return;
+		}
+			// this is the section that actually inserts the values into the User table
+		if(getDBValues('reg_id') == ''){
+			db.transaction(function(transaction) {
+				transaction.executeSql('INSERT INTO User(key, value) VALUES (?,?)',['reg_id', result],
+				nullHandler,errorHandler);
+			});
+		}
     },
     errorHandler:function(error) {
         alert(error);
@@ -82,6 +116,49 @@ var app = {
                 alert('An unknown GCM event has occurred');
                 break;
         }
-    }
+    },
+	AddValueToDB:function(key,value) {
+		if (!window.openDatabase) {
+			alert('Databases are not supported in this browser.');
+			return;
+		}
+			// this is the section that actually inserts the values into the User table
+		db.transaction(function(transaction) {
+			transaction.executeSql('INSERT INTO User(key, value) VALUES (?,?)',[key, value],nullHandler,errorHandler);
+		});
+			// this calls the function that will show what is in the User table in the database 
+			//ListDBValues();
+		return false;
+	},
+	nullHandler:function(){},
+	successCallBack:function() {
+		//alert("DEBUGGING: success");
+	},
+	getDBValues:function(key) {
+		if (!window.openDatabase) {
+			alert('Databases are not supported in this browser.');
+			return;
+		}
 
+			// this line clears out any content in the #lbUsers element on the page so that the next few lines will show updated content and not just keep repeating lines
+		db.transaction(function(transaction) {
+			transaction.executeSql("SELECT * FROM tnet_login_details WHERE key = '".key."';", [],function(transaction, result)
+			{
+				$('#lbUsers').html('');
+				if (result != null && result.rows != null) {
+					/* for (var i = 0; i < result.rows.length; i++) {
+						var row = result.rows.item(i);
+						$('#lbUsers').append('<br>' + row.UserId + '. ' +row.FirstName+ ' ' + row.LastName);
+					} */
+					var row = result.rows.item(0);
+					result = row.value;
+					$('#lbUsers').append('<br>' + row.UserId + '. ' +row.key+ ' ' + row.value);
+				}
+				else{
+					result = '';
+				}
+			},errorHandler);
+		},errorHandler,nullHandler);
+		return result;
+	}
 };
