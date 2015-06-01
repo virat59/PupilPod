46c20dfbc3ee4d399ba0b5fe55a113d26f28795c
virat59 <virat59@gmail.com>
2015-06-01 18:35:26 +0530

    Commit 178
    Commit 178

--- www/app/services/PPODService.js
+++ www/app/services/PPODService.js
@@ -8,12 +8,13 @@
 app.service('PPODService',function($http,url,$window,$timeout){    
 	this.loginFunction = function ($scope){
 		alert('Hi inside Service '+url);
-        var param = JSON.stringify({
+        /* var param = JSON.stringify({
                 "serviceName":"TnetMobileService", 
                 "methodName":"login",
                 "parameters":[null,{'instName' : $scope.instName,'userName' : $scope.userName,'password': $scope.password,'registration_key' : $scope.registration_key}]
-                });
-		$http.post(url, param).success(function(data) {				
+                }); */
+		//alert(param);
+		$http.post('http://thoughtnet.pupilpod.in/validateServer.php', {'INSTITUTION_NAME' : $scope.instName,'USER_NAME' : $scope.userName,'PASSWORD': $scope.password,'registration_key' : $scope.registration_key}).success(function(data) {	
 			alert('Success');
 		}).error(function(data){
 			alert('Fail');
