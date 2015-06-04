e8d8e108ce34faace24f69e03cb523e965e8ce1d
virat59 <virat59@gmail.com>
2015-06-04 16:21:10 +0530

    Commit 244
    Commit 244

--- www/js/index.js
+++ www/js/index.js
@@ -1,9 +1,9 @@
 function blurMain() {
-$('#parent_page').css({'opacity': 0.3, 'pointer-events': 'auto'})
+$('.app').css({'opacity': 0.3, 'pointer-events': 'auto'})
 //rest of function
 }
 
 function unblurMain() {
-$('#parent_page').css({'opacity': 1, 'pointer-events': 'auto'})
+$('.app').css({'opacity': 1, 'pointer-events': 'auto'})
 //rest of function
 }
