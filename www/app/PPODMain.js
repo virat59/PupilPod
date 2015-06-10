3dcce47531424b929c675ad02b32d42f794023be
virat59 <virat59@gmail.com>
2015-06-10 14:30:06 +0530

    COmmit 288
    COmmit 288

--- www/app/PPODMain.js
+++ www/app/PPODMain.js
@@ -1,4 +1,4 @@
-var app = angular.module('PPOD',['ngRoute','mobile-angular-ui','mobile-angular-ui.gestures','pushNotifications.ctrl',"ngCordova"]);
+var app = angular.module('PPOD',['ngRoute','mobile-angular-ui','mobile-angular-ui.gestures','pushNotifications.ctrl',"ngCordova",'ngAnimate']);
 
 app.constant('url', 'NBA/amfphp-2.1/Amfphp/?contentType=application/json');
 
