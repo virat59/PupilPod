1c18eba236a5da79c556f3741eacabc927939dac
virat59 <virat59@gmail.com>
2015-06-11 15:43:50 +0530

    Commit 304
    Commit 304

--- www/app/controllers/PPODController.js
+++ www/app/controllers/PPODController.js
@@ -3,6 +3,19 @@ app.controller('PPODController',function($scope,PPODService,$http,$window,$docum
 	initialize();
 	$scope.loginTrue = sharedProperties.getIsLogin();
 	$scope.student_name = "Virat Joshi";
+	$scope.colours = [
+		{
+			name: "Red",
+			hex: "#F21B1B"
+		}, {
+			name: "Blue",
+			hex: "#1B66F2"
+		}, {
+			name: "Green",
+			hex: "#07BA16"
+		}
+	];
+	$scope.colour = "";
 	function initialize() {
 		$scope.ngViewClass = "modalOff";
 		//alert('Hi In initialize');
@@ -117,6 +130,55 @@ app.controller('PPODController',function($scope,PPODService,$http,$window,$docum
 	};
 	
 	
+});
+
+app.run(function($rootScope) {
+	angular.element(document).on("click", function(e) {
+		$rootScope.$broadcast("documentClicked", angular.element(e.target));
+	});
+});
+
+app.directive("dropdown", function($rootScope) {
+	return {
+		restrict: "E",
+		templateUrl: "app/directives/templates/dropdown.html",
+		scope: {
+			placeholder: "@",
+			list: "=",
+			selected: "=",
+			property: "@"
+		},
+		link: function(scope) {
+			scope.listVisible = false;
+			scope.isPlaceholder = true;
+
+			scope.select = function(item) {
+				scope.isPlaceholder = false;
+				scope.selected = item;
+			};
+
+			scope.isSelected = function(item) {
+				return item[scope.property] === scope.selected[scope.property];
+			};
+
+			scope.show = function() {
+				scope.listVisible = true;
+			};
+
+			$rootScope.$on("documentClicked", function(inner, target) {
+				console.log($(target[0]).is(".dropdown-display.clicked") || $(target[0]).parents(".dropdown-display.clicked").length > 0);
+				if (!$(target[0]).is(".dropdown-display.clicked") && !$(target[0]).parents(".dropdown-display.clicked").length > 0)
+					scope.$apply(function() {
+						scope.listVisible = false;
+					});
+			});
+
+			scope.$watch("selected", function(value) {
+				scope.isPlaceholder = scope.selected[scope.property] === undefined;
+				scope.display = scope.selected[scope.property];
+			});
+		}
+	}
 });
 
 app.directive('dragToDismiss', function($drag, $parse, $timeout){
