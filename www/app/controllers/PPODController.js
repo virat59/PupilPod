d0e84dad5a3f1178b92ff67d25ecde27643d0b10
virat59 <virat59@gmail.com>
2015-06-22 17:00:50 +0530

    Commit 359
    Commit 359

--- www/app/controllers/PPODController.js
+++ www/app/controllers/PPODController.js
@@ -411,8 +411,9 @@ app.controller('mainController',function($scope,PPODService,$http,$window,$docum
 		}
 		$scope.$emit('modelOffEvent', true);
     }
-	
-	$scope.onReload = function() {
+	$scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+
+    $scope.onReload = function() {
       console.warn('reload');
       var deferred = $q.defer();
       setTimeout(function() {
